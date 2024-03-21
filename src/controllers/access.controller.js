ea16df798ade9e682f82707f101cb95d9e25b5f1
dangdev0402 <dangdev0402@gmail.com>
2024-03-22 02:01:57 +0700

    Add Handler SuccessResponse

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,14 @@
 "use strict";
 
+import { CREATED } from "../core/success.response.js";
 import { AccessService } from "../services/access.service.js";
 
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(201).json(await AccessService.SignUp(req.body));
+    new CREATED({
+      message: "Registered OK",
+      metadata: await AccessService.SignUp(req.body),
+    }).send(res);
   };
 }
 export const accessController = new AccessController();
